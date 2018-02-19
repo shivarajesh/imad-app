bf222cbf3d554f9fdfc9789f4575e0e646e5f8fb
shivarajesh <36514933+shivarajesh@users.noreply.github.com>
2018-02-20 00:24:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-text'
 element.innerHTML = "new value";
 
 //move the image
-var img = document.getElementById("img");
+var img = document.getElementById("modi");
 img.onclick = function(){
    img.style.marginleft = '100px';
 } ;
