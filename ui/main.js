18d58f27709b6ff26bed51e8299fe8e0729edef4
shivarajesh <36514933+shivarajesh@users.noreply.github.com>
2018-02-20 00:40:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,11 @@ element.innerHTML = "new value";
 
 //move the image
 var img = document.getElementById('modi');
+var marginLeft = '0';
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
-    var interval = setinterval
-   img.style.marginLeft = '100px';
+    var interval = setInterval(moveLeft,100);
 } ;
